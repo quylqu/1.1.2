e2d10911f50b0346d701fade410f0aa080369966
A.Sh <qqquylqu@gmail.com>
2023-12-16 17:30:18 +0300

    commit 1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,15 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        //Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+        Util util = new Util();
+        util.getConnection();
+
+
     }
+
 }
+
